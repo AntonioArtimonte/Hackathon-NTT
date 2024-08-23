43b2af33cee5cec7c02b82287bc914384f3256f2
olin-med <olin.costa@sou.inteli.edu.br>
2024-08-23 18:46:32 -0300

    feat: adds frontend project

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="flex justify-between w-full h-screen">
+
+      <div className="flex flex-col justify-center w-1/2 bg-[#04584a] h-full items-center">
+        <h1 className="text-white text-4xl font-bold">RogersAI</h1>
+      </div>
+      
+
+      <div className="flex flex-col justify-center w-1/2 items-center">
+        <h1 className="text-[#04584a] text-4xl font-bold">O Importante é Participar</h1>
+      </div>
+    </div>
+  )
+}
