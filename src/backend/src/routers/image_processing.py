4fdb711546be98bb424abbb6e34b947e25049255
Antonio <123978882+AntonioArtimonte@users.noreply.github.com>
2024-08-24 19:00:37 -0300

    add yolomodel backend

--- src/backend/src/routers/image_processing.py
+++ src/backend/src/routers/image_processing.py
@@ -10,6 +10,3 @@ async def process_image_endpoint(data: ImageData = Body(...)):
         return await process_image(data)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
-
-# A fazer com o modelo
