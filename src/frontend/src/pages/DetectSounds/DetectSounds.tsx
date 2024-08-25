309780b3df08698957cdcbdffd66e3dbfc8ec8da
ItsVasconcellos <fernando.asv@uol.com.br>
2024-08-25 01:00:13 -0300

    feat: button and partial waves

--- src/frontend/src/pages/DetectSounds/DetectSounds.tsx
+++ src/frontend/src/pages/DetectSounds/DetectSounds.tsx
@@ -1,10 +1,14 @@
-import React, { Component, createRef, RefObject } from 'react';
-import Waveform from 'waveform-data';
-import Peaks from 'peaks.js';
+import React, { Component, createRef, RefObject } from "react";
+import Waveform from "waveform-data";
+import Peaks from "peaks.js";
 
-import { createPointMarker, createSegmentMarker } from './scripts/MarkerFactories';
-import { createSegmentLabel } from './SegmentLabelFactory';
-import Navbar from '../../components/Navbar';
+import {
+  createPointMarker,
+  createSegmentMarker,
+} from "./scripts/MarkerFactories";
+import { createSegmentLabel } from "./SegmentLabelFactory";
+import Navbar from "../../components/Navbar";
+import Button from "../../components/Button";
 
 // Define the Props and State interfaces for the component
 interface DetectSoundsProps {
@@ -16,7 +20,10 @@ interface DetectSoundsState {
 }
 
 // Define the DetectSounds component
-export default class DetectSounds extends Component<DetectSoundsProps, DetectSoundsState> {
+export default class DetectSounds extends Component<
+  DetectSoundsProps,
+  DetectSoundsState
+> {
   zoomviewWaveformRef: RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
   overviewWaveformRef: RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
   audioElementRef: RefObject<HTMLAudioElement> = createRef<HTMLAudioElement>();
@@ -24,24 +31,62 @@ export default class DetectSounds extends Component<DetectSoundsProps, DetectSou
 
   render() {
     return (
-      <div className='h-screen bg-black flex flex-col'>
+      <div className="h-screen bg-black flex flex-col">
         <Navbar />
-        <div className='flex justify-center'>
-          <div
-            className="zoomview-container"
-            ref={this.zoomviewWaveformRef}
-          ></div>
-          <div
-            className="overview-container"
-            ref={this.overviewWaveformRef}
-          ></div>
+        <div className="flex justify-center ">
+          <div className="flex flex-col justify-center items-center rounded-2xl p-44 bg-white w-3/4 gap-y-4">
+            <h1 className="text-4xl mb-4">Envie o áudio</h1>
+            <form
+              onSubmit={handleSubmit}
+              className="flex border-2 border-black rounded-xl items-center"
+            >
+              <label
+                htmlFor="file-upload"
+                className="bg-gray-700 text-white text-lg py-2 px-4 rounded-l-lg cursor-pointer"
+              >
+                Escolher Arquivo
+              </label>
+              <input
+                id="file-upload"
+                type="file"
+                accept="image/*"
+                onChange={handleFileChange}
+                className="hidden"
+              />
+              {file && (
+                <p className="text-black py-2 px-4">
+                  <div className="text-lg">{file.name}</div>
+                </p>
+              )}
+              <button
+                type="submit"
+                className="bg-black text-white text-lg py-2 px-8 rounded-r-lg"
+              >
+                Submit
+              </button>
+            </form>
+            <div
+              className="zoomview-container gap-y-4"
+              ref={this.zoomviewWaveformRef}
+            ></div>
+            <div
+              className="overview-container"
+              ref={this.overviewWaveformRef}
+            ></div>
 
-          <audio ref={this.audioElementRef} controls>
-            <source src="" type="" />
-            Your browser does not support the audio element.
-          </audio>
+            <div className="flex flex-row">
+              <audio
+                ref={this.audioElementRef}
+                className="gap-y-4 mr-4"
+                controls
+              >
+                <source src="" type="" />
+                Your browser does not support the audio element.
+              </audio>
 
-          {this.renderButtons()}
+              {this.renderButtons()}
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -49,6 +94,36 @@ export default class DetectSounds extends Component<DetectSoundsProps, DetectSou
 
   // Dummy implementation of renderButtons
   renderButtons() {
-    return <div>Buttons</div>;
+    return (
+      <>
+        <Button
+          content={"Aproximar"}
+          functionality={() => {
+            console.log("Teste");
+          }}
+        >
+          Zoom in
+        </Button>
+        <Button
+          content={"Diminuir"}
+          functionality={() => {
+            console.log("Teste");
+          }}
+        >
+          Zoom out
+        </Button>
+        <Button content={"Adicionar Marcação"} functionality={() => {}}>
+          Add Point
+        </Button>
+        <Button
+          content={"Log"}
+          functionality={() => {
+            console.log("Teste");
+          }}
+        >
+          Log segments/points
+        </Button>
+      </>
+    );
   }
 }
