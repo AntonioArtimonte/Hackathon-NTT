e54dc47667a797bae087061931322191f1db6e38
olin-med <olin.costa@sou.inteli.edu.br>
2024-08-24 21:33:40 -0300

    toma-fiotinho

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Landing from './pages/LandingPage'
+import Predict from './pages/Predict'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/main" element={<App />} />
+        <Route path="/predict" element={<Predict />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
