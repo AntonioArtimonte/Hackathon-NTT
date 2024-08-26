613caebe067b4eaa864737eec157ccbb3d9321b8
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-08-25 21:26:30 -0300

    feat: processamento de vídeo

--- src/backend/src/routers/__init__.py
+++ src/backend/src/routers/__init__.py
@@ -3,6 +3,7 @@ from .crud import router as crud
 from .route_processing import router as route_processing
 from .image_processing import router as image_processing
 from .audio_processing import router as audio_processing
+from .video_processing import router as video_processing
 
 router = APIRouter()
 
@@ -11,3 +12,4 @@ router.include_router(crud, prefix="/crud")
 router.include_router(route_processing, prefix="/route_processing")
 router.include_router(image_processing, prefix="/image_processing")
 router.include_router(audio_processing, prefix="/audio_processing")
+router.include_router(video_processing, prefix="/video_processing")
