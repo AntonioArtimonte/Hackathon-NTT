78a781ca6920edf966029d12f3ecac31cbc1d5ec
ItsVasconcellos <fernando.asv@uol.com.br>
2024-08-25 21:44:55 -0300

    feat: ronquinho

--- src/frontend/src/pages/DetectSounds/DetectSounds.tsx
+++ src/frontend/src/pages/DetectSounds/DetectSounds.tsx
@@ -1,129 +1,81 @@
-import React, { Component, createRef, RefObject } from "react";
-import Waveform from "waveform-data";
-import Peaks from "peaks.js";
-
-import {
-  createPointMarker,
-  createSegmentMarker,
-} from "./scripts/MarkerFactories";
-import { createSegmentLabel } from "./SegmentLabelFactory";
+import React, { useRef, useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Button from "../../components/Button";
 
-// Define the Props and State interfaces for the component
-interface DetectSoundsProps {
-  // Add any props that DetectSounds might receive here
-}
+const DetectSounds: React.FC = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [selectedFile, setSelectedFile] = useState<string | undefined>(
+    undefined
+  );
 
-interface DetectSoundsState {
-  // Add any state properties for DetectSounds here
-}
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setFile(event.target.files[0]);
+      setSelectedFile(URL.createObjectURL(event.target.files[0]));
+    }
+  };
 
-// Define the DetectSounds component
-export default class DetectSounds extends Component<
-  DetectSoundsProps,
-  DetectSoundsState
-> {
-  zoomviewWaveformRef: RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
-  overviewWaveformRef: RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
-  audioElementRef: RefObject<HTMLAudioElement> = createRef<HTMLAudioElement>();
-  peaks: Peaks | null = null;
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!file) return;
+    const bufferFile = await file.arrayBuffer();
+    const fileBase64 = Buffer.from(bufferFile).toString('base64');
+    try {
+      const response = await fetch("http://localhost:8000/api/audio_processing/analyze_audio", {
+        method: "POST",
+        body: JSON.stringify({ audio_base64: fileBase64 }),
+      });
 
-  render() {
-    return (
-      <div className="h-screen bg-black flex flex-col">
-        <Navbar />
-        <div className="flex justify-center ">
-          <div className="flex flex-col justify-center items-center rounded-2xl p-44 bg-white w-3/4 gap-y-4">
-            <h1 className="text-4xl mb-4">Envie o áudio</h1>
-            <form
-              onSubmit={handleSubmit}
-              className="flex border-2 border-black rounded-xl items-center"
+      if (response.ok) {
+        console.log(response.body)
+        console.log("Audio uploaded successfully!");
+      } else {
+        console.error("Failed to upload audio.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div className="h-screen bg-black flex flex-col">
+      <Navbar />
+      <div className="flex justify-center">
+        <div className="flex flex-col justify-center items-center rounded-2xl p-44 bg-white w-1/2 gap-y-4">
+          <h1 className="text-4xl mb-4">Envie o áudio</h1>
+          <form
+            onSubmit={handleSubmit}
+            className="flex border-2 border-black rounded-xl items-center"
+          >
+            <label
+              htmlFor="file-upload"
+              className="bg-gray-700 text-white text-lg py-2 px-4 rounded-l-lg cursor-pointer"
             >
-              <label
-                htmlFor="file-upload"
-                className="bg-gray-700 text-white text-lg py-2 px-4 rounded-l-lg cursor-pointer"
-              >
-                Escolher Arquivo
-              </label>
-              <input
-                id="file-upload"
-                type="file"
-                accept="image/*"
-                onChange={handleFileChange}
-                className="hidden"
-              />
-              {file && (
-                <p className="text-black py-2 px-4">
-                  <div className="text-lg">{file.name}</div>
-                </p>
-              )}
-              <button
-                type="submit"
-                className="bg-black text-white text-lg py-2 px-8 rounded-r-lg"
-              >
-                Submit
-              </button>
-            </form>
-            <div
-              className="zoomview-container gap-y-4"
-              ref={this.zoomviewWaveformRef}
-            ></div>
-            <div
-              className="overview-container"
-              ref={this.overviewWaveformRef}
-            ></div>
-
-            <div className="flex flex-row">
-              <audio
-                ref={this.audioElementRef}
-                className="gap-y-4 mr-4"
-                controls
-              >
-                <source src="" type="" />
-                Your browser does not support the audio element.
-              </audio>
-
-              {this.renderButtons()}
-            </div>
-          </div>
+              Escolher Arquivo
+            </label>
+            <input
+              id="file-upload"
+              type="file"
+              accept="audio/wav"
+              onChange={handleFileChange}
+              className="hidden"
+            />
+            {file && (
+              <p className="text-black py-2 px-4">
+                <div className="text-lg">{file.name}</div>
+              </p>
+            )}
+            <button
+              type="submit"
+              className="bg-black text-white text-lg py-2 px-8 rounded-r-lg"
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+};
 
-  // Dummy implementation of renderButtons
-  renderButtons() {
-    return (
-      <>
-        <Button
-          content={"Aproximar"}
-          functionality={() => {
-            console.log("Teste");
-          }}
-        >
-          Zoom in
-        </Button>
-        <Button
-          content={"Diminuir"}
-          functionality={() => {
-            console.log("Teste");
-          }}
-        >
-          Zoom out
-        </Button>
-        <Button content={"Adicionar Marcação"} functionality={() => {}}>
-          Add Point
-        </Button>
-        <Button
-          content={"Log"}
-          functionality={() => {
-            console.log("Teste");
-          }}
-        >
-          Log segments/points
-        </Button>
-      </>
-    );
-  }
-}
+export default DetectSounds;
